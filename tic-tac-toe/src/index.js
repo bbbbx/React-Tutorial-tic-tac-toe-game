08907eb514cc546507fc5fb9049bdc5c7e4dd2ea
bbbbx <venus@venusworld.cn>
2017-08-29 20:05:08 +0800

    :sparkles:井字游戏 winner 背景高亮

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -20,22 +20,16 @@ class Board extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+      <div className="board" ref="rows">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
       </div>
     );
   }
@@ -46,9 +40,11 @@ class Game extends React.Component {
     super();
   
     this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
+      history: [
+        {
+          squares: Array(9).fill(null),
+        }
+      ],
       stepNumber: 0,
       xIsNext: true,
     };
@@ -58,9 +54,12 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+
+    //游戏结束
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+
     squares[i] = this.state.xIsNext ? '✘' : 'O';
     this.setState({
       history: history.concat([{
@@ -93,14 +92,18 @@ class Game extends React.Component {
     })
     let status;
     if (winner) {
-      status = '胜者是: ' + winner;
+      status = '胜者是: ' + current.squares[winner[0]];
+      //更改赢的方块的 className 
+      this.refs.Board.refs.rows.children[winner[0]].className="square wonSquare";
+      this.refs.Board.refs.rows.children[winner[1]].className="square wonSquare";
+      this.refs.Board.refs.rows.children[winner[2]].className="square wonSquare";
     } else {
       status = '下一个玩家是: ' + (this.state.xIsNext ? '✘' : 'O');
     }
     return (
       <div className="game">
         <div className="game-board">
-          <Board 
+          <Board ref="Board"
             squares = {current.squares}
             onClick = {(i) => this.handleClick(i)}
           />
@@ -123,12 +126,12 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
+    [2, 4, 6]
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return new Array(a, b, c);
     }
   }
   return null;
